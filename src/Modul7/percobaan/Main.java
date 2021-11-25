f65f4a3106d71cab088dd9066173abbf0cae2785
babyblue <elanbagaskoro182@gmail.com>
2021-11-25 21:42:22 +0700

    Modul 7

---
+++ src/Modul7/percobaan/Main.java
@@ -0,0 +1,10 @@
+package Modul7.percobaan;
+
+public class Main {
+    public static void main(String[] args) {
+        Mahasiswa aku = new Mahasiswa();
+
+        aku.info();
+        aku.info("Bambang");
+    }
+}
---
+++ src/Modul7/percobaan/Penguin.java
@@ -0,0 +1,7 @@
+package Modul7.percobaan;
+
+public class Penguin {
+    public void terbang() {
+        System.out.println("Aku tidak bisa terbang");
+    }
+}
