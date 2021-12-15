85d171b51c00041325ed4f3bc5ee7eaf546f7fc1
babyblue <elanbagaskoro182@gmail.com>
2021-12-15 19:47:00 +0700

    Revert "Modul 7"
    This reverts commit 080f6f79694e8608db4d0fff1bc48ce0741ee4c4.

---
+++ src/Modul7/percobaan/C.java
@@ -0,0 +1,4 @@
+package Modul7.percobaan;
+
+public class C extends B{
+}
---
+++ src/Modul7/percobaan/Mahasiswa.java
@@ -0,0 +1,11 @@
+package Modul7.percobaan;
+
+public class Mahasiswa {
+    public void info() {
+        System.out.println("Haloo Aku Mahasiswa!");
+    }
+
+    public void info(String nama) {
+        System.out.println("Haloo Namaku " + nama);
+    }
+}
