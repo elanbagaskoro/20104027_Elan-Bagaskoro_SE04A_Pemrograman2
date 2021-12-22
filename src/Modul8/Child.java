95f559c98a5c29f59be7d45d38a8d77296c79323
babyblue <77684867+sugarbooo@users.noreply.github.com>
2021-12-22 22:08:47 +0700

    Delete src/Modul8 directory

--- src/Modul8/Child.java
+++
@@ -1,8 +0,0 @@
-package Modul8;
-
-public class Child extends Parent{
-    int x = 10;
-    public void info() {
-        System.out.println("ini class child");
-    }
-}
--- src/Modul8/Tes.java
+++
@@ -1,9 +0,0 @@
-package Modul8;
-
-public class Tes {
-    public static void main(String[] args) {
-        Parent tes = new Child();
-        System.out.println("nilai x = " + tes.x);
-        tes.info();
-    }
-}
